993394d3f11408705c9056dfd6b75166f78cbb0f
sroej <denkidev4@gmail.com>
2026-01-15 10:04:25 +0000

    Update and rename config.json to config.js

---
+++ database/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  tokens: "8456275541:AAEXe2ob4qcXyZzMDlsX4B6IwrtkK62EgZU",  // Masukin Bot token kamu
+  owners: "6796038285", // Masukin ID Telegram kamu
+  port: "", // Masukin Port panel kamu 
+  ipvps: "" // Masukin IP vps kamu atau domain panel kamu yg asalnya ( https://AiiSigma.id ) menjadi ( http://AiiSigma.id )
+};
+
+/* WAJIB BACA !!!
+ cari di index.js halaman 1995-1996 di situ masukin bot token sama id tele lu.
+
+ emngnya knpa bang?
+ lu buka aja dulu. di sana udah gw kasi penjelasannya
+*/
